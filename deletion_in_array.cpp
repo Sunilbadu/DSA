cb29a826ec2ace57856fa9a50e789db26a9a7d47
Sunilbadu <sunylbadu7@gmail.com>
2026-06-15 19:48:02 +0545

    update

--- deletion_in_array.cpp
+++ deletion_in_array.cpp
@@ -4,9 +4,7 @@ int main(){
 	int i, pos,val,n=4;
 	int arr[10]={10,20,30,40,50};
 	
-	for(i=0;i<=n;i++){
-		cout<<"The value at the index" << i <<"is " << arr[i]<<endl;
-	}
+
 	
 	cout << "Enter the index of the element you want to delete";
 	cin >> pos;
@@ -19,7 +17,7 @@ int main(){
 	
 		for(i=0;i<=
 		n;i++){
-		cout<<"The value at the index" << i <<"is " << arr[i]<<endl;
+		cout<< arr[i]<<"  ";
 	}
 	return 0;
 }
