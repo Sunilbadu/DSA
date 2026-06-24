f2f17c1d9fbccee9cb312b7b9f4ae6ad795b3313
Sunilbadu <sunylbadu7@gmail.com>
2026-06-24 21:01:08 +0545

    junktest

--- stack.cpp
+++ stack.cpp
@@ -1,65 +1,56 @@
 #include <iostream>
 using namespace std;
+#define MAX 5  
+int stack[MAX];
 
-#define MAX 5   // maximum size of stack
+int top=-1;
 
-class Stack {
-    int arr[MAX];
-    int top;
-
-public:
-    Stack() { top = -1; }
-
-    // Push operation
-    void push(int x) {
+    void push(int value) {
         if (top == MAX - 1) {
-            cout << "Stack Overflow\n";
+            cout << "Stack Overflow \n";
             return;
         }
-        arr[++top] = x;
-        cout << x << " pushed into stack\n";
+        else{
+        top++;
+        stack[top] = value;
+        cout << value << " pushed into stack \n";
+		}
+      
     }
 
-    // Pop operation
     void pop() {
         if (top == -1) {
             cout << "Stack Underflow\n";
             return;
         }
-        cout << arr[top--] << " popped from stack\n";
+         else{
+        cout << stack[top] << " popped from stack \n";
+        top--;
+		 }
+   
+      
     }
 
-    // Peek operation
-    void peek() {
-        if (top == -1) {
-            cout << "Stack is Empty\n";
-            return;
-        }
-        cout << "Top element: " << arr[top] << endl;
-    }
-
-    // Display stack
     void display() {
         if (top == -1) {
-            cout << "Stack is Empty\n";
+            cout << "Stack is Empty \n";
             return;
         }
-        cout << "Stack elements: ";
-        for (int i = 0; i <= top; i++) {
-            cout << arr[i] << " ";
+        else{
+        	 cout << "Stack elements: "<<endl;
+        for (int i = top; i >= 0; i--) {
+            cout << stack[i] << endl;
         }
         cout << endl;
+		}
+       
     }
-};
-
-int main() {
-    Stack s;
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    s.display();
-    s.peek();
-    s.pop();
-    s.display();
+int main() {  
+    push(10);
+    push(20);
+    push(30);
+    display();
+    pop();
+    display();
     return 0;
 }
