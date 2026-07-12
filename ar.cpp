1cb69b078d45d13852819b8c70f959c2eb9fd152
Sunilbadu <sunylbadu7@gmail.com>
2026-07-12 21:52:29 +0545

    final update

--- ar.cpp
+++ ar.cpp
@@ -2,23 +2,24 @@
 using namespace std;
 
 int main() {
-    int arr[5];   // Declare an array of size 5
+    const int size = 5;
+    int arr[size];
 
-    cout << "Enter 5 integers: ";
-    for (int i = 0; i < 5; i++) {
-        cin >> arr[i];   // Input values into the array
+    cout << "Enter " << size << " integers: ";
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
     }
 
     cout << "\nYou entered: ";
-    for (int i = 0; i < 5; i++) {
-        cout << arr[i] << " ";   // Display array elements
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
     }
 
-    // Example: Find sum of array elements
     int sum = 0;
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < size; ++i) {
         sum += arr[i];
     }
+
     cout << "\nSum of elements = " << sum << endl;
 
     return 0;
