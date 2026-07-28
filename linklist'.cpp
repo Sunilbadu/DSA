14afb600e9040d7eba32e5030e00e3c4eb2af4a5
Sunilbadu <sunylbadu7@gmail.com>
2026-07-28 10:24:54 +0545

    linked list

--- linklist'.cpp
+++ linklist'.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Node
@@ -13,29 +13,20 @@ Node* head = NULL;
 void insertFront(int value)
 {
     Node* newNode = new Node();
-
-    newNode->data = value;
+    newNode->data =value;
     newNode->next = head;
-
     head = newNode;
 }
 
 void insertEnd(int value)
 {
-    Node* newNode = new Node();
-
+    Node* newNode= new Node();
     newNode->data = value;
-    newNode->next = NULL;
-
-    if(head == NULL)
-    {
-        head = newNode;
-        return;
-    }
-
+    newNode->next =NULL;
+    
     Node* temp = head;
 
-    while(temp->next != NULL)
+    while(temp->next!= NULL)
     {
         temp = temp->next;
     }
@@ -43,18 +34,40 @@ void insertEnd(int value)
     temp->next = newNode;
 }
 
-void deleteFront()
+void insertAtPosition(int value, int position)
 {
-    if(head == NULL)
+    Node* newNode = new Node();
+    newNode->data = value;
+
+    if(position == 1)
     {
-        cout<<"List Empty"<<endl;
+        newNode->next = head;
+        head = newNode;
         return;
     }
 
     Node* temp = head;
 
-    head = head->next;
+    for(int i =1; i < position - 1; i++)
+    {
+        temp = temp->next;
+    }
 
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+
+void deleteFront()
+{
+    if(head == NULL)
+    {
+        cout << "List is Empty" << endl;
+        return;
+    }
+
+    Node* temp = head;
+    head = head->next;
     delete temp;
 }
 
@@ -62,7 +75,7 @@ void deleteEnd()
 {
     if(head == NULL)
     {
-        cout<<"List Empty"<<endl;
+        cout << "List is Empty" << endl;
         return;
     }
 
@@ -83,39 +96,75 @@ void deleteEnd()
     }
 
     prev->next = NULL;
+    delete temp;
+}
+void deleteAtPosition(int position)
+{
+    if(head == NULL)
+    {
+        cout << "List is Empty" << endl;
+        return;
+    }
 
+    if(position == 1)
+    {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return;
+    }
+
+    Node* temp = head;
+    Node* prev = NULL;
+
+    for(int i = 1; i < position; i++)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    prev->next = temp->next;
     delete temp;
 }
 
 void display()
 {
+    if(head == NULL)
+    {
+        cout << "List is Empty" << endl;
+        return;
+    }
+
     Node* temp = head;
 
     while(temp != NULL)
     {
-        cout<<temp->data<<" -> ";
-
+        cout << temp->data << " -> ";
         temp = temp->next;
     }
 
-    cout<<"NULL"<<endl;
+    cout << "NULL" << endl;
 }
 
 int main()
 {
-	  display();
     insertFront(20);
     insertFront(10);
-    insertEnd(30);
     insertEnd(40);
-
+    insertEnd(50);
+    cout << "Initial List:" << endl;
     display();
-
+    insertAtPosition(30, 3);
+    cout << "\nAfter Inserting 30 at Position 3:" << endl;
+    display();
     deleteFront();
+    cout << "\nAfter Deleting from Beginning:" << endl;
     display();
-
     deleteEnd();
+    cout << "\nAfter Deleting from End:" << endl;
     display();
-
+    deleteAtPosition(2);
+    cout << "\nAfter Deleting Position 2:" << endl;
+    display();
     return 0;
 }
